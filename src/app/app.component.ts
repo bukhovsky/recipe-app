3bb9699a60663d55202c0345d33d9bdcc5fca5cb
bukhovsky <bukhovsky@gmail.com>
2023-04-13 17:18:27 +0300

    product add form add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,5 +25,27 @@ export class AppComponent {
       .catch(error => console.error(error));
 
   } 
+  
+  add_product_button_function (engName: string, ukrName:string, fat:string, carbohydrates:string, protein: string) {
+    const newObj = {
+      engName: engName, 
+      ukrName: ukrName, 
+      fat: fat, 
+      carbohydrates: carbohydrates, 
+      protein: protein,
+    }
+    fetch('https://worker-ts5.bukhovskyi.workers.dev/add', {
+      method: 'POST',
 
+      //don't work with header O_o -> try to find out why?
+      // headers: {
+      //    'Content-Type': 'application/json'
+      //  },
+      body: JSON.stringify(newObj)
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+    console.log(newObj)
+  }
 }
